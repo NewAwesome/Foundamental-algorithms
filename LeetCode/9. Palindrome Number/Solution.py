ac6171c3125d9f23da3dab3612c211edd3861769
jiayufei <jiayufei@360.cn>
2021-02-01 19:07:36 +0800

    feat: 修复分支代码回归正轨

--- LeetCode/9. Palindrome Number/Solution.py
+++ LeetCode/9. Palindrome Number/Solution.py
@@ -1,11 +1,8 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-<<<<<<< HEAD
         # If x is negative, return false directly
         if x < 0:
             return False
-=======
->>>>>>> ec3c0cf527a648c6e1e951bcb7b3485f2a2b8adc
         temp = str(x)
         for i in (0, int(len(temp) / 2)):
             if temp[i] != temp[len(temp) - 1 - i]:
